21a61a2eb7e6b2c6581cb30e5f31d7665af330eb
mjakerick <mjakerick@gmail.com>
2019-06-08 10:59:56 -0700

    added pet info into Pet component

--- paws-frontend/src/components/Pet.js
+++ paws-frontend/src/components/Pet.js
@@ -4,6 +4,14 @@ class Pet extends Component {
   render() {
     return (
       <div className="pet">
+        <div className="pet-info">
+          {this.props.pet.name}
+          {this.props.pet.species}
+          {this.props.pet.breed}
+          {this.props.pet.photo}
+          {this.props.pet.last_known_location}
+          {this.props.pet.description}
+        </div>
         this is the Pet component
       </div>
     )
