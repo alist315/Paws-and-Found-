6eee43d75050bca79c8f13557358add79f560ad5
mjakerick <mjakerick@gmail.com>
2019-06-08 09:58:53 -0700

    initial components setup

---
+++ paws-frontend/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        this is the Navigation component
+      </div>
+    )
+  }
+}
+
+export default Navigation;
---
+++ paws-frontend/src/components/PetList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PetList extends Component {
+  render() {
+    return (
+      <div>
+        this is the PetList component
+      </div>
+    )
+  }
+}
+
+export default PetList;
