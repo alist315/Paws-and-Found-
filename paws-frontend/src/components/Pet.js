630fd854aa7d5633a7332fab1874cf197595471e
alist315 <aaron.reinheimner315@gmail.com>
2019-06-08 18:18:18 -0400

    changes

--- paws-frontend/src/components/Pet.js
+++ paws-frontend/src/components/Pet.js
@@ -4,16 +4,16 @@ class Pet extends Component {
   render() {
     return (
       <div className="pet">
-        <div className="pet-info">
+        <div className="pet-info">{this.props.pet.lost_pet}</div>
           {this.props.pet.name}
           {this.props.pet.species}
           {this.props.pet.breed}
           {this.props.pet.photo}
           {this.props.pet.last_known_location}
           {this.props.pet.description}
-        </div>
         this is the Pet component
-      </div>
+        </div>
+
     )
   }
 }
