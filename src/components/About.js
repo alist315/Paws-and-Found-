d4dc33670b87cefbd3d8d7b0993ee1142a55814b
Jake <mjakerick@gmail.com>
2019-06-10 09:44:34 -0700

    Added some text to the About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        this is the About component
+        Paws-and-Found is a place for pet owners to post flyers for their missing pets. Think of Paws-and-Found as the neighborhood telephone pole, but instead of only reaching people who happen to notice your flyer on a street corner, Paws-and-Found has the ability to reach anyone who visits our site. We know how important your pets are to you and that's why we are dedicated to reuniting pets with their families and making a paws-itive impact in our community.
       </div>
     )
   }
